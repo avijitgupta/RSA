2257e4b57d6e6cc22e6d2220bbaa2ae99d2df360
Avijit Gupta <mailforavijit@gmail.com>
2012-10-11 19:47:18 -0400

    test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+
+for i in {1..100}
+do
+./rsaengine -genrsa -pubout public_key.der -privout priv_key.der
+./rsaengine -encrypt -pubin public_key.der -in message -out encrypted_message
+./rsaengine -decrypt -privin priv_key.der -in encrypted_message -out decrypted_msg
+./rsaengine -sign -privin priv_key.der -in message -out signature_file -certout public_certificate.der
+./rsaengine -verify -signature signature_file -certin public_certificate.der -in message 
+done
